a8aeb19f28ddd488135f60f6f6b2ae2e3eaf4985
illustris <rharikrishnan95@gmail.com>
2018-08-29 18:37:44 +0530

    add support for reading remote hosts from a list

--- benchmark.sh
+++ benchmark.sh
@@ -60,110 +60,116 @@ log_arping() {
 	cat arping.log | sed -n -e 's/.*rtt\s[a-z\/-]*\s=\s[0-9.]*\/\([0-9.]*\)\/.*/\1/p' >> arping.log
 }
 
-if [ $# -lt 4 ]
-then
-	echo -n "Usage: "
-	echo -n $0
-	echo " [destination IP] [network interface] [period] [count] [(optional): remote logs[y/N]]"
-	exit
-fi
+run() {
+	remoteport=''
+	remotelogging=$5
 
-sudo id # Just to acquire sudo
-
-remoteport=''
-remotelogging=$5
-
-# if remote logging is enabled, trigger logging
-if [ $remotelogging == 'y' ]
-then
-	echo "sending trigger for remote logging"
-	remoteport=$(./nc -d $1 $rport)
-	if [ -z "$remoteport" ]
+	# if remote logging is enabled, trigger logging
+	if [ $remotelogging == 'y' ]
 	then
-		echo "Failed to get remote port"
-		remotelogging='n'
+		printf "sending trigger to %s for logging on port %s\n" "$1" "$rport"
+		remoteport=$(./nc -w 5 -d $1 $rport)
+		if [ -z "$remoteport" ]
+		then
+			echo "Failed to get remote port"
+			remotelogging='n'
+		fi
 	fi
-fi
 
-log_arping $1 $4 $2 &
-pid1=$!
-printf "arping started with PID %s\n" "$pid1"
-pstree -lp $pid1
-log_ping $1 $4 &
-pid2=$!
-printf "ping started with PID %s\n" "$pid2"
-pstree -lp $pid2
-log_cpu $3 &
-pid3=$!
-printf "cpu logging started with PID %s\n" "$pid3"
-pstree -lp $pid3
-log_net $2 $3 &
-pid4=$!
-printf "network logging started with PID %s\n" "$pid4"
-pstree -lp $pid4
+	log_arping $1 $4 $2 &
+	pid1=$!
+	printf "arping started with PID %s\n" "$pid1"
+	pstree -lp $pid1
+	log_ping $1 $4 &
+	pid2=$!
+	printf "ping started with PID %s\n" "$pid2"
+	pstree -lp $pid2
+	log_cpu $3 &
+	pid3=$!
+	printf "cpu logging started with PID %s\n" "$pid3"
+	pstree -lp $pid3
+	log_net $2 $3 &
+	pid4=$!
+	printf "network logging started with PID %s\n" "$pid4"
+	pstree -lp $pid4
 
-echo "waiting for ping/arping to finish"
-pstree -lp $$
-wait $pid1
-wait $pid2
-echo "killing CPU logging"
-killtree $pid3
-echo "killing net logging"
-killtree $pid4
-pstree -lp $$
+	echo "waiting for ping/arping to finish"
+	pstree -lp $$
+	wait $pid1
+	wait $pid2
+	echo "killing CPU logging"
+	killtree $pid3
+	echo "killing net logging"
+	killtree $pid4
+	pstree -lp $$
 
-# terminate remote logging, fetch remote logs
-if [ $remotelogging == 'y' ]
-then
-	echo "sending trigger to end remote logging"
-	./nc -znv $1 $remoteport
-	sleep 2
-	./nc -d $1 $remoteport > r_tx_$iflog
-	echo "Fetched r_tx log"
-	sleep 2
-	./nc -d $1 $remoteport > r_rx_$iflog
-	echo "Fetched r_rx log"
-	sleep 2
-	./nc -d $1 $remoteport > r_$cpulog
-	echo "Fetched r_cpu log"
-fi
+	# terminate remote logging, fetch remote logs
+	if [ $remotelogging == 'y' ]
+	then
+		echo "sending trigger to end remote logging"
+		./nc -znv $1 $remoteport
+		sleep 2
+		./nc -d $1 $remoteport > r_tx_$iflog
+		echo "Fetched r_tx log"
+		sleep 2
+		./nc -d $1 $remoteport > r_rx_$iflog
+		echo "Fetched r_rx log"
+		sleep 2
+		./nc -d $1 $remoteport > r_$cpulog
+		echo "Fetched r_cpu log"
+	fi
 
-echo "parsing logs"
-pingav=$(cat ping.log | tail -n1)
-arpingav=$(cat arping.log | tail -n1)
+	echo "parsing logs"
+	pingav=$(cat ping.log | tail -n1)
+	arpingav=$(cat arping.log | tail -n1)
 
-rxbytes=$(cat rx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
-txbytes=$(cat tx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
-tbytes=$(expr $rxbytes + $txbytes)
-duration=$(cat tx_$iflog | cut -d":" -f1 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
-tspeed=$(expr $txbytes / $duration)
-rspeed=$(expr $rxbytes / $duration)
-speed=$(expr $tbytes / $duration)
-
-count=$(cat cpu.log | wc -l)
-cputot=$(cat cpu.log | cut -d' ' -f2 | paste -s -d+ - | bc | grep -o "^[0-9]*")
-avcpu=$(expr $cputot / $count)
-
-printf "ping: %s\narping: %s\nTx: %s\nRx: %s\nTot: %s\navCPU:%s\n" "$pingav" "$arpingav" "$tspeed" "$rspeed" "$speed" "$avcpu"
-
-if [ $remotelogging == 'y' ]
-then
-	rxbytes=$(cat r_rx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
-	txbytes=$(cat r_tx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
+	rxbytes=$(cat rx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
+	txbytes=$(cat tx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
 	tbytes=$(expr $rxbytes + $txbytes)
-	duration=$(cat r_tx_$iflog | cut -d":" -f1 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
+	duration=$(cat tx_$iflog | cut -d":" -f1 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
 	tspeed=$(expr $txbytes / $duration)
 	rspeed=$(expr $rxbytes / $duration)
 	speed=$(expr $tbytes / $duration)
 
-	count=$(cat r_cpu.log | wc -l)
-	cputot=$(cat r_cpu.log | cut -d' ' -f2 | paste -s -d+ - | bc | grep -o "^[0-9]*")
+	count=$(cat cpu.log | wc -l)
+	cputot=$(cat cpu.log | cut -d' ' -f2 | paste -s -d+ - | bc | grep -o "^[0-9]*")
 	avcpu=$(expr $cputot / $count)
-	printf "Remote stats:\nTx: %s\nRx: %s\nTot: %s\navCPU:%s\n" "$tspeed" "$rspeed" "$speed" "$avcpu"
+
+	printf "ping: %s\narping: %s\nTx: %s\nRx: %s\nTot: %s\navCPU:%s\n" "$pingav" "$arpingav" "$tspeed" "$rspeed" "$speed" "$avcpu" | tee $1_stats.log
+
+	if [ $remotelogging == 'y' ]
+	then
+		rxbytes=$(cat r_rx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
+		txbytes=$(cat r_tx_$iflog | cut -d" " -f 2 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
+		tbytes=$(expr $rxbytes + $txbytes)
+		duration=$(cat r_tx_$iflog | cut -d":" -f1 | sed -e 1b -e '$!d' | tac | paste -s -d- - | bc)
+		tspeed=$(expr $txbytes / $duration)
+		rspeed=$(expr $rxbytes / $duration)
+		speed=$(expr $tbytes / $duration)
+
+		count=$(cat r_cpu.log | wc -l)
+		cputot=$(cat r_cpu.log | cut -d' ' -f2 | paste -s -d+ - | bc | grep -o "^[0-9]*")
+		avcpu=$(expr $cputot / $count)
+		printf "Remote stats:\nTx: %s\nRx: %s\nTot: %s\navCPU:%s\n" "$tspeed" "$rspeed" "$speed" "$avcpu" | tee -a $1_stats.log
+	fi
+
+
+
+	# generate timeseries for ping and arping
+	cat arping.log | sed -n -e 's/^\([0-9:]*\)\s.*time=\([0-9.]*\).*$/\1\2/p' > ts_arping.log
+	cat ping.log | sed -n -e 's/^\([0-9:]*\)\s.*time=\([0-9.]*\).*$/\1\2/p' > ts_ping.log
+}
+
+if [ $# -lt 4 ]
+then
+	echo -n "Usage: "
+	echo -n $0
+	echo " [host list file] [network interface] [period] [count] [(optional): remote logs[y/N]]"
+	exit
 fi
 
+sudo echo "" # Just to acquire sudo
 
-
-# generate timeseries for ping and arping
-cat arping.log | sed -n -e 's/^\([0-9:]*\)\s.*time=\([0-9.]*\).*$/\1\2/p' > ts_arping.log
-cat ping.log | sed -n -e 's/^\([0-9:]*\)\s.*time=\([0-9.]*\).*$/\1\2/p' > ts_ping.log
+while read rem; do
+	run $rem $2 $3 $4 $5
+done <remotes.list
